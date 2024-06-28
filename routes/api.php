bf882154d69950c5b0775683abc08e7234d16714
martinjoo <martin@martinjoo.dev>
2024-06-28 22:57:34 +0200

    Install sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,13 @@ use App\Http\Controllers\RideController;
 use App\Http\Controllers\UpdateCurrentLocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return 'ok';
-});
+Route::post('/rides', [RideController::class, 'store'])
+    ->name('rides.store')
+    ->middleware('auth:sanctum');
 
 Route::patch('/drivers/{driver}/current-location', UpdateCurrentLocationController::class)->name('drivers.update-current-location');
 Route::patch('/drivers/{driver}/check-in', CheckInController::class)->name('drivers.check-in');
 Route::get('/drivers/{driver}/status', [DriverController::class, 'status']);
-Route::post('/ride', [RideController::class, 'store'])->name('rides.store');
 Route::patch('rides/{ride}/accept', [RideController::class, 'accept'])->name('rides.accept');
 Route::patch('rides/{ride}/pick-up', [RideController::class, 'pickUp'])->name('rides.pick-up');
 Route::patch('rides/{ride}/drop-off', [RideController::class, 'dropOff'])->name('rides.drop-off');
