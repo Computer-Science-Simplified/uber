aa2c0c006c430c2d1677e74ca320ba6fc170e315
martinjoo <martin@martinjoo.dev>
2024-06-28 22:57:47 +0200

    Configure job backoff

--- app/Jobs/NotifyClosestDriver.php
+++ app/Jobs/NotifyClosestDriver.php
@@ -7,7 +7,6 @@ use App\Notifications\RideRequestedNotification;
 use App\Services\LocationService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -16,16 +15,18 @@ class NotifyClosestDriver implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $tries = 4;
+
+    public $backoff = [10, 20, 30];
+
     public function __construct(private readonly Ride $ride)
     {
     }
 
     public function handle(LocationService $locationService): void
     {
-        try {
-            $closestDriver = $locationService->getClosestDriver($this->ride);
+        $closestDriver = $locationService->getClosestDriver($this->ride);
 
-            $closestDriver->notify(new RideRequestedNotification($this->ride));
-        } catch (ModelNotFoundException) {}
+        $closestDriver->notify(new RideRequestedNotification($this->ride));
     }
 }
