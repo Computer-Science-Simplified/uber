efa3db8399b3be12387c47f62b6359c3a91dfe6c
martinjoo <martin@martinjoo.dev>
2024-07-01 19:48:54 +0200

    Clean up

--- app/Enums/RideStatus.php
+++ app/Enums/RideStatus.php
@@ -5,8 +5,11 @@ namespace App\Enums;
 enum RideStatus: string
 {
     case Waiting = 'waiting';
+
     case Accepted = 'accepted';
+
     case InProgress = 'in-progress';
+
     case Finished = 'finished';
 
     case Abandoned = 'abandoned';
